895882cce61db42a5ea9d8bc4db27213d3f2b1c1
Rishabh Shukla <rishabh.shukla@acoustte.com>
2023-01-26 16:42:11 +0530

    adding json server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,17 @@ import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackContext from '../shared/FeedbackContext'
 import FeedbackItem from "./FeedbackItem"
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
   
-    if(!feedback || feedback.length===0){
+    if( !isLoading && (!feedback || feedback.length===0)){
         return <p>No Feedback Yet</p>
     }
     // WITH ANIMATION
-    return (
+
+    return isLoading? <Spinner /> : (
       <div className="feedback-list"> 
       <AnimatePresence>
       {feedback.map((item) => (
@@ -27,6 +29,23 @@ function FeedbackList() {
       </AnimatePresence>
       </div>
     )
+    // return (
+    //   <div className="feedback-list"> 
+    //   <AnimatePresence>
+    //   {feedback.map((item) => (
+    //     <motion.div
+    //     key={item.id}
+    //     initial={{ opacity : 0}}
+    //     animate= { { opacity : 1 }}
+    //     exit= { { opacity : 0 }}
+    //     >
+    //       <FeedbackItem key={item.id} item={item}
+    //       />
+    //       </motion.div>
+    //   ))}
+    //   </AnimatePresence>
+    //   </div>
+    // )
 
 
   // WITHOUT ANY ANIMATION
